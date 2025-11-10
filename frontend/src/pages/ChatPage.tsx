da79b193c972bb6ef8292affe726b36356a367f8
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-11-10 10:37:14 +0530

    Login page created

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,16 +1,36 @@
-import { useAuthStore } from "../store/useAuthStore"
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import BorderAnimatedContainer from "../components/BorderAnimator";
+import ChatContainer from "../components/ChatContainer";
+import ChatList from "../components/ChatList";
+import ContactList from "../components/ContactList";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
+import ProfileHeader from "../components/ProfileHeader";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
 
 const ChatPage = () => {
+  // global store -----
 
-  const {logOut}= useAuthStore()
-
-  const handleLogout = () => {
-    logOut()
-  }
+  const { activeTab, selectedUser } = useChatStore();
   return (
-    <div className="z-10">
-      <button onClick={handleLogout} className="btn btn-primary m-10">Logout</button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* Left side */}
+
+        <div className="w-8- bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop:blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
+      </BorderAnimatedContainer>
     </div>
-  )
-}
-export default ChatPage
+  );
+};
+export default ChatPage;
---
+++ frontend/src/store/useChatStore.tsx
@@ -0,0 +1,87 @@
+import { create } from "zustand";
+import { api } from "../services/api";
+import toast from "react-hot-toast";
+
+export interface chatType {
+  _id: string;
+  userId: string;
+}
+
+export interface MessageType {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  text?: string;
+  image?: string;
+  createdAt: string;
+}
+
+export interface useChatStoreType {
+  allContacts: string[] | null;
+  chats: chatType[] | null;
+  messages: MessageType[];
+  activeTab: "chats" | "contacts";
+  selectedUser: chatType | null;
+  loading: boolean;
+  isSoundEnabled: boolean;
+  toggleButton: () => void;
+  setActiveTab: (tab: "chats" | "contacts") => void;
+  setSelectedUser: (selectedUser: chatType) => void;
+  getAllContacts: () => Promise<void>;
+  getMyChatPartners: () => Promise<void>;
+}
+
+export const useChatStore = create<useChatStoreType>((set, get) => ({
+  allContacts: [],
+  chats: [],
+  messages: [],
+  activeTab: "chats",
+  selectedUser: null,
+  loading: false,
+  isSoundEnabled: localStorage.getItem("isSoundEnabled") === "true",
+
+  toggleButton: () => {
+    localStorage.setItem("isSoundEnabled", String(!get().isSoundEnabled));
+    set({ isSoundEnabled: !get().isSoundEnabled });
+  },
+
+  setActiveTab: (tab) => {
+    set({ activeTab: tab });
+  },
+
+  setSelectedUser: (selectedUser) => {
+    set({ selectedUser });
+  },
+
+  getAllContacts: async () => {
+    try {
+      set({ loading: true });
+      const { data } = await api.get("/contacts", {});
+      set({ allContacts: data.contacts });
+    } catch (error: any) {
+      const errorMessage =
+        error.response.data.message || error.message || "Something went wrong";
+      toast.error(errorMessage);
+      set({ allContacts: null });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  getMyChatPartners: async () => {
+    try {
+      set({ loading: true });
+      const { data } = await api.get("/chats", {});
+      set({ chats: data.chatPatners });
+    } catch (error: any) {
+      const errorMessage =
+        error.response.data.message || error.message || "Something went wrong";
+      toast.error(errorMessage);
+      set({ chats: null });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  
+}));
